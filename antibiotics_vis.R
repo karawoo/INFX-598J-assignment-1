a95b1f40718297c646ad0392616f783a1aac9532
Kara Woo <woo.kara@gmail.com>
2016-01-08 21:56:22 -0800

    Two version of plot - one with hue, one with shape

--- antibiotics_vis.R
+++ antibiotics_vis.R
@@ -19,18 +19,34 @@ dat_long$gram.staining <- factor(dat_long$Gram.Staining,
                                  labels = c("Gram-negative bacteria",
                                             "Gram-positive bacteria"))
 
-####################
-####  Dot plot  ####
-####################
+################
+####  Plot  ####
+################
 
-ggplot(dat_long, aes(x = mic, y = Bacteria, color = Antibiotic)) +
-  geom_point(size = 5, alpha = 0.7) +
-  scale_color_manual(values = wes_palette("Darjeeling")) + 
+baseplot <- ggplot(dat_long, aes(x = mic, y = Bacteria)) +
+  annotation_logticks(base = 10, sides = "b", color = "gray") +
   scale_x_log10(labels = scales::comma) +
   facet_grid(gram.staining ~ ., scales = "free_y", labeller = label_value) +
   ylab("Bacteria") +
-  xlab(expression(log("Minimum Inhibitory Concentration"))) +
-  ggtitle("Antibiotic effectiveness by bacteria") +
+  xlab("Minimum Inhibitory Concentration") +
+  ggtitle("Antibiotic Effectiveness") +
   theme_bw() +
-  ggsave("figs/antibiotics_dot_plot.png", width = 10, height = 6)
+  theme(axis.text = element_text(size = 10),
+        strip.text.y = element_text(size = 10),
+        axis.title = element_text(size = 13),
+        plot.title = element_text(size = 20))
+
+## Version with hue for antibiotic type
+
+baseplot +
+  geom_point(aes(color = Antibiotic), size = 6, alpha = 0.65) +
+  scale_color_manual(values = wes_palette("Darjeeling")) + 
+  ggsave("figs/antibiotics_plot_hue.png", width = 10, height = 6)
  
+## Version with shape for antibiotic type
+
+baseplot +
+  geom_point(aes(shape = Antibiotic), size = 6) +
+  scale_shape_manual(values = c(1, 0, 3)) +
+  ggsave("figs/antibiotics_plot_shape.png", width = 10, height = 6)
+  
